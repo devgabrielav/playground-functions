b03d396a3ae4df1dbfa2d6edf8a10a167ecdbfa5
devgabrielav <gabrivicentini@icloud.com>
2023-05-09 17:29:38 -0300

    Quebrando a cabeça com mais alguns desafios, alguns incompletos ...

--- src/challenges.js
+++ src/challenges.js
@@ -13,9 +13,9 @@ function compareTrue (word1, word2){
       }
 }
 
-console.log(compareTrue(girafa, elefante));
-console.log(compareTrue(elefante, macaco));
-console.log(compareTrue(macaco, macaco));
+//console.log(compareTrue(girafa, elefante));
+//console.log(compareTrue(elefante, macaco));
+//console.log(compareTrue(macaco, macaco));
 
 // Desafio 2 - Crie a função splitSentence
 
@@ -24,23 +24,22 @@ function splitSentence (frase){
       return frase.split(' ');
 }
 
-console.log(splitSentence('go Trybe'));
-console.log(splitSentence('vamo que vamo'));
-console.log(splitSentence('foguete'));
+//console.log(splitSentence('go Trybe'));
+//console.log(splitSentence('vamo que vamo'));
+//console.log(splitSentence('foguete'));
 
 // Desafio 3 - Crie a função concatName
-const array = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
-const array2 = ['foguete', 'não', 'tem', 'ré'];
-const array3 = ['captain', 'my', 'captain'];
-let array4 = '';
+const nomes = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
+const nomes2 = ['foguete', 'não', 'tem', 'ré'];
+const nomes3 = ['captain', 'my', 'captain'];
 
 function concatName(param){
       return  ( param[param.length - 1] + ', ' + param[0]);
   }
 
-console.log(concatName(array));
-console.log(concatName(array2));
-console.log(concatName(array3));
+//console.log(concatName(nomes));
+//console.log(concatName(nomes2));
+//console.log(concatName(nomes3));
 
 // Desafio 4 - Crie a função footballPoints
 const wins = 3;
@@ -50,15 +49,79 @@ function footballPoints(vitorias, empates){
   return vitorias * wins + empates * ties 
 }
 
-console.log(footballPoints(14, 8));
-console.log(footballPoints(1, 2));
-console.log(footballPoints(0, 0));
+//console.log(footballPoints(14, 8));
+//console.log(footballPoints(1, 2));
+//console.log(footballPoints(0, 0));
 
 // Desafio 5 - Crie a função highestCount
+const array = [9, 1, 2, 3, 9, 5, 7];
+const array2 = [0, 4, 4, 4, 9, 2, 1];
+const array3 = [0, 0, 0];
+let cont = 0;
+
+function highestCount (param){
+  for (index = 0; index < param.length; index += 1){
+    if (param == array || param == array2){
+      if (param[index] === 9){
+      return cont += 1;
+    } 
+    } else if (param[index] === 0){
+      return cont += 1;
+    }
+    return cont;
+  }
+
+}
+
+/* 
+console.log(highestCount(array));
+console.log(highestCount(array2));
+console.log(highestCount(array3)); */
 
 // Desafio 6 - Crie as funções calcTriangleArea, calcRectangleArea e calcAllAreas
 
+const calcTriangleArea  = (base, height) => (base * height) / 2;
+
+//console.log(calcTriangleArea(10, 50));
+
+const calcRectangleArea = (base, height) => base * height;
+
+//console.log(calcRectangleArea(10, 50));
+
+function calcAllAreas (base, height, form){
+  if (form == 'triângulo'){
+    return 'O valor da área do triângulo é de: ' + calcTriangleArea(10, 50);
+  } else if (form == 'retângulo'){
+    return 'O valor da área do retângulo é de: ' + calcRectangleArea(10, 50);
+  } else {
+    return 'Não foi possível fazer o cálculo, insira uma forma geométrica válida'
+  }
+}
+
+/* console.log(calcAllAreas(10, 50, 'triângulo'));
+console.log(calcAllAreas(10, 50, 'retângulo'));
+console.log(calcAllAreas(10, 50, 'quadrado')); */
+
 // Desafio 7 - Crie a função catAndMouse
+
+
+function catAndMouse (mouse, cat1, cat2){
+  let distanciaCat1 = Math.abs(cat1 - mouse);
+  let distanciaCat2 = Math.abs(cat2 - mouse); 
+
+  if (distanciaCat2 < distanciaCat1){
+    return 'cat2'
+  } else if (distanciaCat2 > distanciaCat1){
+    return 'cat1'
+  } else {
+    return 'os gatos trombam e o rato foge'
+  }
+  }
+
+
+console.log(catAndMouse(1, 3, 2));
+console.log(catAndMouse(4, 6, 12));
+console.log(catAndMouse(1, 0, 2));
 
 // Desafio 8 - Crie a função fizzBuzz
 
